f353010682b112aee22e6dc4a70e3deb95bd5dc4
JODY MC LAUGHLIN <jmclaughlin31@qub.ac.uk>
2017-02-09 18:42:18 +0000

    Remove MVC

--- PortfolioSite/PowerBI.aspx.cs
+++ PortfolioSite/PowerBI.aspx.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿
+using System;
 using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 using Microsoft.PowerBI.Api.V1;
 using Microsoft.PowerBI.Security;
 using Microsoft.Rest;
