274ea0dbef3a8e30fb897d0c8a6e61fae7205cd2
JODY MC LAUGHLIN <jmclaughlin31@qub.ac.uk>
2017-02-09 18:22:16 +0000

    update to route config attempt 3

--- PortfolioSite/App_Start/RouteConfig.cs
+++ PortfolioSite/App_Start/RouteConfig.cs
@@ -14,10 +14,16 @@ namespace PortfolioSite
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                    "",
+                    "home/default.aspx",
+                        new { controller = "Home", action = "Default" }
+                );
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
